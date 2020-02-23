cbd2940a40e1b0611a04e043bf537795f462cbee
Ninganza Remy <36136892+ninganzaremy@users.noreply.github.com>
2020-02-23 00:20:50 -0500

    Add files via upload

---
+++ suits_pages/main.js
@@ -0,0 +1,7 @@
+$('.menu-open-icon').click(function () {
+    $('.main-nav-outer').addClass('open-menu');
+});
+
+$('.menu-close-icon').click(function () {
+    $('.main-nav-outer').removeClass('open-menu');
+});
