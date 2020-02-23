a49d057a19b4b3a2efe597ca8f2d7597afb3cf29
Ninganza Remy <36136892+ninganzaremy@users.noreply.github.com>
2020-02-23 00:17:29 -0500

    Add files via upload

---
+++ visit_eac/js/popupwind.js
@@ -0,0 +1,38 @@
+
+     //code for portfolio openings
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
+
+  function openNav2() {
+      document.getElementById("myNav2").style.width = "100%";
+    }
+
+    function closeNav2() {
+      document.getElementById("myNav2").style.width = "0%";
+    }
+
+    function openNav3() {
+        document.getElementById("myNav3").style.width = "100%";
+      }
+
+      function closeNav3() {
+        document.getElementById("myNav3").style.width = "0%";
+      }
+
+
+      function openNav4() {
+          document.getElementById("myNav4").style.width = "100%";
+        }
+
+        function closeNav4() {
+          document.getElementById("myNav4").style.width = "0%";
+        }
+    //code for safari  hover_end
+(function(l){var i,s={touchend:function(){}};for(i in s)l.addEventListener(i,s)})(document);
