a6189a18a5d4f88ab2ff1a0fd3fd9de4895b1f6d
Ninganza Remy <36136892+ninganzaremy@users.noreply.github.com>
2020-02-22 23:42:09 -0500

    Add files via upload

---
+++ electronics_app/ajax-content/ajax_post.php
@@ -0,0 +1,25 @@
+  <?php
+  echo '<div class="element-item">
+                        <div class="tt-post">
+                            <div class="tt-post-img">
+                                <a href="blog-single-post.html"><img src="images/blog/masonry/masonry-img-01.jpg" alt=""></a>
+                            </div>
+                            <div class="tt-post-content">
+                                <div class="tt-background"></div>
+                                <div class="tt-tag"><a href="#">FASHION</a></div>
+                                <h2 class="tt-title"><a href="blog-single-post.html">DOLORE EU FUGIATNULLA PARIATUR</a></h2>
+                                <div class="tt-descrip/Users/remy/Desktop/remy_portfolio/electronics_app/ajax-content/ajax_product.phption">
+                                    Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </div>
+                                <div class="tt-meta">
+                                    <div class="tt-autor">
+                                        by <span>ADRIAN</span> on January 14, 2017
+                                    </div>
+                                    <div class="tt-comments">
+                                        <a href="#"><i /Users/remy/Desktop/remy_portfolio/electronics_app/ajax-content/ajax_product_metro.phpclass="tt-icon icon-h-11"></i>7</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>';
+?>
